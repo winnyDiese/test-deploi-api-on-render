20025aa02d9053e70c7a644aead21a024c65522b
winnyDiese <winnydiese@gmail.com>
2024-08-01 14:18:03 +0200

    dflkjzbdfzrboiub

--- app.js
+++ app.js
@@ -5,18 +5,9 @@ const app = express()
 
 const port = 3000
 
-// const router = express.Router()
-
-connect_mongodb()
-
-// router.get('/',(req,res)=>{
-//     res.json({
-//         "Pascal-message":"Salut Boss...",
-//         "Pascal-message-2":"Armel ton api est deja en ligne..."
-//     })
-// })
 
 const routerAgence = require('./routers/agence')
-app.use('/agence',routerAgence)
+app.use('/api/agence',routerAgence)
 
 app.listen(port, console.log('Console running on '+port+' port'))
+
--- routers/agence.js
+++ routers/agence.js
@@ -1,16 +1,10 @@
 
 const express = require('express')
 const Agence = require('../models/agence')
+const connect_mongodb = require('../lib/connect_db')
 const router = express.Router()
 
-// router.route('/').get(async (req,res)=>{
-//     try {
-//         const agences = await Agence.find()
-//         res.json(agences)
-//     } catch (error) {
-//         res.status(500).send(error.message)
-//     }
-// })
+connect_mongodb()
 
 router.get('/', async (req,res)=>{
     try {
