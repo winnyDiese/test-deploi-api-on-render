50d8d4be4cb346d8e9b58c8cfc82c81556bcf0c8
winnyDiese <winnydiese@gmail.com>
2024-08-01 17:59:17 +0200

    creating api user...

---
+++ models/user.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({ 
+    // password: String,
+    // email: String,
+    // username: String
+
+    nomUser:String,
+    fonctionAgent:String,
+    phoneUser:String,
+    passwordUser:String,
+    emailUser:String,
+    adresseUSer:String,
+    sexe:String,
+    id_typeUser:String,
+    id_ville:String,
+    statutUser:String
+    
+},{timestamps:true})
+
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+// export default User
+
+module.exports = User
