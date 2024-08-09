2237f82a7da8bc203c19ae68188a62a25cbbe2e1
winnyDiese <winnydiese@gmail.com>
2024-08-10 00:28:32 +0200

    sdlfkjbdfvbeibkjhb

--- routers/pays.js
+++ routers/pays.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const { all_pays, add_pays,one_contry,delete_contry } = require('../controllers/pays')
+const { all_pays, add_pays,one_contry,delete_contry, update_contry } = require('../controllers/pays')
 const router = express.Router()
 
 
@@ -8,5 +8,6 @@ router.get('/pays', all_pays)
 router.post('/pays', add_pays)
 router.get('/pays/:id', one_contry)
 router.delete('/pays/:id', delete_contry)
+router.put('/pays/:id', update_contry)
 
 module.exports = router
