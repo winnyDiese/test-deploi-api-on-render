1dbeb52c9162451cb28b9543d575624688b7e292
winnyDiese <winnydiese@gmail.com>
2024-08-10 08:35:38 +0200

    sdlfkvjbdlfvb

--- controllers/destination.js
+++ controllers/destination.js
@@ -16,9 +16,9 @@ const all_destination = async (req,res)=>{
 }
 
 const add_destination = async (req,res)=>{
-    const {nomType} = await req.body
+    const {id_villeA,id_villeB} = await req.body
     
-    const new_destination = new Destination({nomType})
+    const new_destination = new Destination({id_villeA,id_villeB})
 
     try {
         const saved_destination = await new_destination.save()
@@ -34,7 +34,7 @@ const delete_destination = async (req,res) => {
     try {
         const deleted_destination = await Destination.findByIdAndDelete(id)
 
-        if (!deleted_destination) return res.status(404).json({ message: 'destination non, trouvé !' });
+        if (!deleted_destination) return res.status(404).json({ message: 'Destination non, trouvé !' });
         res.status(200).json({ message: 'Un destination a été supprimé avec sucées !', destination: deleted_destination });
 
     } catch (error) {
@@ -51,9 +51,9 @@ const update_destination = async (req,res) => {
         const updates = req.body
 
         const updated_destination = await Destination.findByIdAndUpdate(id, updates, {new:true})
-        if(!updated_destination) return res.status(404).json({message:"destination non trouvé !"})
+        if(!updated_destination) return res.status(404).json({message:"Destination non trouvé !"})
         
-        res.status(200).json({message:"destination mise à jour avec succées !", destination: updated_destination})
+        res.status(200).json({message:"Destination mise à jour avec succées !", destination: updated_destination})
     } catch (error) {
         console.log(error)
         res.status(500).json(error.message)
