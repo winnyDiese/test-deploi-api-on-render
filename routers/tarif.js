efba874ebede6ed10b4a148164d323f0615f6c1f
winnyDiese <winnydiese@gmail.com>
2024-08-10 16:56:41 +0200

    sdfvljkbdofviuefbvoiub

--- routers/tarif.js
+++ routers/tarif.js
@@ -1,12 +1,14 @@
 
 const express = require('express')
-const { all_tarif, add_tarif,one_tarif,delete_tarif } = require('../controllers/tarif')
+const { all_tarif, add_tarif,one_tarif,delete_tarif,update_tarif } = require('../controllers/tarif')
 const router = express.Router()
 
 
 router.get('/tarif', all_tarif)
 router.post('/tarif', add_tarif)
-router.get('/tarif/:id', one_tarif)
 router.delete('/tarif/:id', delete_tarif)
+router.put('/tarif/:id', update_tarif)
+
+router.get('/tarif/:id', one_tarif)
 
 module.exports = router
