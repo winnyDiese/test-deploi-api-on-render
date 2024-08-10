c1fd53ec11cdea0b09dd3bc75f00a99b4c0f42ea
winnyDiese <winnydiese@gmail.com>
2024-08-10 07:46:15 +0200

    sdkfjvbeoibuoiu

---
+++ routers/type-user.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const { all_type, add_type,delete_type,update_type,one_type } = require('../controllers/pays')
+const router = express.Router()
+
+
+router.get('/type-user', all_type)
+router.post('/type-user', add_type)
+router.delete('/type-user/:id', delete_type)
+router.put('/type-user/:id', update_type)
+
+router.get('/type-user/:id', one_type)
+
+module.exports = router
