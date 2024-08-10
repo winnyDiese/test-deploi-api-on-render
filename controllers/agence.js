88b4b38d155a8dc9bc05555ae56c55a46932b22f
winnyDiese <winnydiese@gmail.com>
2024-08-10 09:47:29 +0200

    lfkjbsldfkbjliub

--- controllers/agence.js
+++ controllers/agence.js
@@ -29,4 +29,53 @@ const add_agence = async (req,res) => {
     }
 }
 
-module.exports = {all_agence, add_agence}
+const delete_agence = async (req,res) => {
+    const { id } = req.params;
+    try {
+        const deleted_agence = await Agence.findByIdAndDelete(id)
+
+        if (!deleted_agence) return res.status(404).json({ message: 'Agence non, trouvé !' });
+        res.status(200).json({ message: 'Un Agence a été supprimé avec sucées !', agence: deleted_agence });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error.message)
+    }
+
+}
+
+const update_agence = async (req,res) => {
+    
+    try {
+        const {id} = req.params
+        const updates = req.body
+
+        const updated_agence = await Agence.findByIdAndUpdate(id, updates, {new:true})
+        if(!updated_agence) return res.status(404).json({message:"Agence non trouvé !"})
+        
+        res.status(200).json({message:"Agence mise à jour avec succées !", agence: updated_agence})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error.message)
+    }
+}
+
+const one_agence = async (req,res) => {
+    const { id } = req.params;
+    try {
+        const agence = await Agence.findById(id)
+    
+        if (!agence) return res.status(404).json({ message: 'Agence non, trouvé !' });
+        res.status(200).json(agnece)
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error.message)
+    }
+
+}
+
+
+
+
+module.exports = {all_agence, add_agence, delete_agence, update_agence, one_agence}
