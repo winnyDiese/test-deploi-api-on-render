a1d2f9125f51f9b15c6ee5dcb6daed9c231e67fa
winnyDiese <winnydiese@gmail.com>
2024-08-21 11:44:46 +0100

    lkjb

--- routers/story-colis.js
+++ routers/story-colis.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const { all_story_colis, add_story_colis, delete_story_colis, update_story_colis, one_story_colis } = require('../controllers/story-colis')
+const { all_story_colis, add_story_colis, delete_story_colis, update_story_colis, one_story_colis,story_colis_bycolis,story_colis_bystatus } = require('../controllers/story-colis')
 const router = express.Router()
 
 
@@ -10,5 +10,7 @@ router.delete('/historique-colis/:id', delete_story_colis)
 router.put('/historique-colis/:id', update_story_colis)
 
 router.get('/historique-colis/:id', one_story_colis)
+router.get('/historique-colis/colis/:id', story_colis_bycolis)
+router.get('/historique-colis/status/:id', story_colis_bystatus)
 
 module.exports = router
