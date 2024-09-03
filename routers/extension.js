0c5e9707e90a4061cbaf10150c6ef277d2a59d43
winnyDiese <winnydiese@gmail.com>
2024-09-03 08:45:26 +0200

    jhv

--- routers/extension.js
+++ routers/extension.js
@@ -1,11 +1,12 @@
 
 const express = require('express')
-const {all_extension, add_extension,delete_extension,update_extension,one_extension,one_extension_by_agence,one_extension_by_ville,get_active_extensions_by_agence,get_inactive_extensions_by_agence} = require('../controllers/extension')
+const {all_extension, add_extension,delete_extension,update_extension,one_extension,one_extension_by_agence,one_extension_by_ville,get_active_extensions_by_agence,get_inactive_extensions_by_agence,get_extension_by_id} = require('../controllers/extension')
 const router = express.Router()
 
 
 router.get('/extension', all_extension)
 router.post('/extension', add_extension)
+router.get('/extension/:id', get_extension_by_id)
 router.delete('/extension/:id', delete_extension)
 router.put('/extension/:id', update_extension)
 
