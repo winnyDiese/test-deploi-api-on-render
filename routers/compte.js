d57ac5069f894d22de545c6856288028bdbabaef
winnyDiese <winnydiese@gmail.com>
2024-09-03 13:23:22 +0200

    mlkjnljkb

--- routers/compte.js
+++ routers/compte.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const {all_compte, add_compte, delete_compte, update_compte, one_compte, get_comptes_by_agence} = require('../controllers/compte')
+const {all_compte, add_compte, delete_compte, update_compte, one_compte, get_comptes_by_agence,update_and_add_montant_compte_by_agence} = require('../controllers/compte')
 const router = express.Router()
 
 
@@ -9,6 +9,7 @@ router.post('/compte', add_compte)
 router.delete('/compte/:id', delete_compte)
 router.put('/compte/:id', update_compte)
 
+router.put('/compte/additional/:id_agence', update_and_add_montant_compte_by_agence)
 router.get('/compte/:id', one_compte)
 router.get('/compte/agence/:id_agence', get_comptes_by_agence)
 
