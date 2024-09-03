89b463f7f7f5841dba525e2cef69f19871e44c0b
winnyDiese <winnydiese@gmail.com>
2024-09-04 00:51:55 +0200

    lkjbiku

--- models/tarif.js
+++ models/tarif.js
@@ -8,7 +8,10 @@ const tarifSchema = new mongoose.Schema({
 
     prix:String,
     dateTarif:String,
-    id_agence_dest:String
+    id_agence_dest:{
+        type: mongoose.Schema.Types.ObjectId, // Définit id_destination comme un ObjectId
+        ref: 'Destination' // Référence à la collection Destination
+    }
 },{ timestamps: true })
 
 const Tarif = mongoose.models.Tarif || mongoose.model('Tarif',tarifSchema)
