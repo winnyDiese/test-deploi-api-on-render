6e6cd604cb1a9aa3f2408bffff9764ef8bed5453
winnyDiese <winnydiese@gmail.com>
2024-09-04 14:16:09 +0200

    lkjbljhb

--- models/agence-destination.js
+++ models/agence-destination.js
@@ -5,8 +5,16 @@ const agenceDestinationSchema = new mongoose.Schema({
     // villeA: String,
     // villeB: String
 
-    id_agence:String,
-    id_destination:String,
+    id_agence: {
+        type: Schema.Types.ObjectId,
+        ref: 'Agence', // Reference to the Agence collection
+        required: true
+    },
+    id_destination: {
+        type: Schema.Types.ObjectId,
+        ref: 'Destination', // Reference to the Destination collection
+        required: true
+    },
     statutDest:String
     
 },{ timestamps: true })
