24cd7e10fcf65c80b04036c1edd90d6e4dc5af69
winnyDiese <winnydiese@gmail.com>
2024-09-04 09:24:49 +0200

    ljblv

--- models/destination.js
+++ models/destination.js
@@ -5,8 +5,14 @@ const destinationSchema = new mongoose.Schema({
     // villeA: String,
     // villeB: String
 
-    id_villeA:String,
-    id_villeB:String
+    id_villeA: {
+        type: mongoose.Schema.Types.ObjectId, // Définit id_villeA comme ObjectId
+        ref: 'Ville' // Référence à la collection Ville
+    },
+    id_villeB: {
+        type: mongoose.Schema.Types.ObjectId, // Définit id_villeB comme ObjectId
+        ref: 'Ville' // Référence à la collection Ville
+    }
 },{ timestamps: true })
 
 const Destination = mongoose.models.Destination || mongoose.model('Destination',destinationSchema)
